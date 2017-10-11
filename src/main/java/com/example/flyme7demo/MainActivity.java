5f2a139aa8dc6676c95b9bfd99cbe476904acdcc
eli <3168694142@qq.com>
2017-10-11 20:17:34 +0800

    update

--- src/main/java/com/example/flyme7demo/MainActivity.java
+++ src/main/java/com/example/flyme7demo/MainActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.example.flyme7demo.R;
 import com.drivemode.settings.DriveModeSettingsActivity;
 import com.map.demo.MapActivity;
 import com.musicplayer.demo.MediaActivity;
